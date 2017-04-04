24e452888e7fe5fba38bb620c9d2aa09bea6fa8a
Eric Evans <eevans@wikimedia.org>
2017-04-04 15:14:12 -0500

    Read by revision

--- src/main/java/org/wikimedia/cassandra/Main.java
+++ src/main/java/org/wikimedia/cassandra/Main.java
@@ -88,6 +88,12 @@ public class Main {
         private int numPartitions = 1000;
         @Option(name = { "-po", "--partition-offset" }, description = "Partition offset to start from (default: 0)")
         private int partOffset = 0;
+        @Option(
+                name = { "-nr", "--num-revisions" },
+                description = "Number of revisions to write per-partition (default: 10000)")
+        private int numRevisions = 10000;
+        @Option(name = { "-ro", "--revision-offset" }, description = "Revision offset to start from (default: 0)")
+        private int revOffset = 0;
         @Option(name = "--concurrency", description = "Request concurrency (default: 10)")
         private int concurrency = 10;
 
@@ -98,7 +104,14 @@ public class Main {
             }
 
             try (CassandraSession session = new CassandraSession(this.contact())) {
-                new Reader(metrics, session, this.concurrency, this.numPartitions, this.partOffset).execute();
+                new Reader(
+                        metrics,
+                        session,
+                        this.concurrency,
+                        this.numPartitions,
+                        this.partOffset,
+                        this.numRevisions,
+                        this.revOffset).execute();
             }
             catch (Exception e) {
                 throw Throwables.propagate(e);
--- src/main/java/org/wikimedia/cassandra/Reader.java
+++ src/main/java/org/wikimedia/cassandra/Reader.java
@@ -17,6 +17,7 @@ import org.slf4j.LoggerFactory;
 import com.codahale.metrics.Gauge;
 import com.codahale.metrics.Meter;
 import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.Timer;
 import com.datastax.driver.core.PreparedStatement;
 import com.datastax.driver.core.Statement;
 import com.datastax.driver.core.exceptions.NoHostAvailableException;
@@ -25,33 +26,44 @@ import com.github.rvesse.airline.annotations.Command;
 
 @Command(name = "read", description = "Read data")
 public class Reader {
-    private static final String QUERY = String.format("SELECT rev,tid,value FROM %s.%s WHERE key = ?", KEYSPACE, TABLE);
+    private static final String QUERY = String.format("SELECT rev,tid,value FROM %s.%s WHERE key=? AND rev=?", KEYSPACE, TABLE);
     private static final Logger LOG = LoggerFactory.getLogger(Reader.class);
 
     private final CassandraSession session;
     private final int concurrency;
     private final int numPartitions;
     private final int partitionStart;
+    private final int numRevisions;
+    private final int revisionStart;
     private final int queueSize;
     private final PreparedStatement prepared;
     private final ExecutorService executor;
-    private final Meter attempts;
     private final Meter failures;
+    private final Timer reads;
 
-    public Reader(MetricRegistry metrics, CassandraSession session, int concurrency, int numPartitions, int partOffset) {
+    public Reader(
+            MetricRegistry metrics,
+            CassandraSession session,
+            int concurrency,
+            int numPartitions,
+            int partOffset,
+            int numRevisions,
+            int revOffset) {
         this.concurrency = concurrency;
         this.session = checkNotNull(session);
         this.numPartitions = numPartitions;
         this.partitionStart = partOffset;
+        this.numRevisions = numRevisions;
+        this.revisionStart = revOffset;
 
         this.queueSize = this.concurrency * 2;
         this.prepared = session.prepare(QUERY);
-        this.attempts = metrics.meter(name(Writer.class, "selects", "attempted"));
-        this.failures = metrics.meter(name(Writer.class, "selects", "failed"));
+        this.failures = metrics.meter(name(Reader.class, "failed"));
+        this.reads = metrics.timer(name(Reader.class, "reads"));
 
         final BlockingQueue<Runnable> queue = new LinkedBlockingQueue<>(this.concurrency * 2);
 
-        metrics.register(name(Writer.class, "selects", "enqueued"), new Gauge<Integer>() {
+        metrics.register(name(Reader.class, "selects", "enqueued"), new Gauge<Integer>() {
             @Override
             public Integer getValue() {
                 return queue.size();
@@ -69,29 +81,36 @@ public class Reader {
 
     public void execute() {
         for (int i = this.partitionStart; i < (this.numPartitions + this.partitionStart); i++) {
-            final String key = Writer.keyName(i);
-            executor.submit(() -> {
-                Statement statement = null;
-                try {
-                    statement = this.prepared.bind(key);
-                    this.session.execute(statement);
-                    this.attempts.mark();
-                }
-                catch (NoHostAvailableException | QueryExecutionException e) {
-                    LOG.warn(e.getMessage());
-                    this.failures.mark();
-                }
-                catch (Exception e) {
-                    LOG.error("Unable to execute statement: \"{}\"", statement, e);
-                    this.failures.mark();
-                }
-            });
+            for (int j = this.revisionStart; j < (this.numRevisions + this.revisionStart); j++) {
+                final String key = Writer.keyName(i);
+                final int rev = j;
+                final Timer.Context context = this.reads.time();
+                executor.submit(() -> {
+                    Statement statement = null;
+                    try {
+                        statement = this.prepared.bind(key, rev);
+                        this.session.execute(statement);
+                    }
+                    catch (NoHostAvailableException | QueryExecutionException e) {
+                        LOG.warn(e.getMessage());
+                        this.failures.mark();
+                    }
+                    catch (Exception e) {
+                        LOG.error("Unable to execute statement: \"{}\"", statement, e);
+                        this.failures.mark();
+                    }
+                    finally {
+                        context.stop();
+                    }
+                });
+            }
         }
         LOG.info("All jobs enqueued; Shutting down...");
         executor.shutdown();
 
         try {
-            // Block until any jobs on the queue have completed, or the timeout has expired.
+            // Block until any jobs on the queue have completed, or the timeout
+            // has expired.
             if (!executor.awaitTermination(Math.max(this.queueSize, 180), TimeUnit.SECONDS)) {
                 LOG.warn("Timed out waiting for executor shutdown!");
             }
@@ -99,7 +118,5 @@ public class Reader {
         catch (InterruptedException e) {
             LOG.warn("Interrupted while waiting for executor shutdown", e);
         }
-
     }
-
 }
